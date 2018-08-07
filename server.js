5c0e8dfe865db0196bdaf53c7bc003a5f755e366
lotteje <lotje.j@hotmail.com>
2018-08-07 14:05:57 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const request = require('request');
 const cors = require('cors')
 var parseString = require('xml2js').parseString;
 
-app.use(cors());
-
-app.options('*', cors())
+// app.use(cors());
+let cors = require('cors');
+app.use(cors({ origin: "*" }));
+// app.options('*', cors())
 
 app.use(express.static(__dirname + '/dist/bibVirtualKeyboard'));
 
 app.listen(process.env.PORT || 8080);
 
 app.route('/api/:search').get((req, res) => {
-    var url  = 'http://gent.staging.aquabrowser.be/api/v1/search/?q=' + req.params['search'] + '&authorization='+ process.env.API_KEY;
+    console.log("CALL ONTVANGEN");
+    var url = 'http://gent.staging.aquabrowser.be/api/v1/search/?q=' + req.params['search'] + '&authorization=' + process.env.API_KEY;
     request.get(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             parseString(body, function (err, result) {
